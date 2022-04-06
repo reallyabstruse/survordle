b3d34facf76066e48786be56c470c981129160a1
reallyabstruse <59004470+reallyabstruse@users.noreply.github.com>
2022-04-06 18:33:45 +0200

    share code

--- client/src/shared.js
+++ client/src/shared.js
@@ -1,68 +1,70 @@
-const GREEN = "green";
-const YELLOW = "yellow";
-const RED = "red";
-const BLACK = "black";
-const WHITE = "white";
+(function(exports){
+	const GREEN = "green";
+	const YELLOW = "yellow";
+	const RED = "red";
+	const BLACK = "black";
+	const WHITE = "white";
 
-// Get colors for a single guess
-exports.getColors = function(guess, solution) {
-	if (!guess) {
-		return new Array(solution.length).fill(RED);
-	}
-
-	let colors = new Array(solution.length).fill("");
-	let arr_guess = [...guess];
-	let arr_solution = [...solution];
-
-	for (let i = 0; i < arr_solution.length; i++) {
-		if (arr_guess[i] === arr_solution[i]) {
-			arr_solution[i] = arr_guess[i] = "";
-			colors[i] = GREEN;
+	// Get colors for a single guess
+	exports.getColors = function(guess, solution) {
+		if (!guess) {
+			return new Array(solution.length).fill(RED);
 		}
-	}
 
-	for (let i = 0; i < arr_guess.length; i++) {
-		if (!arr_guess[i]) {
-			continue;
-		}
-		for (let j = 0; j < arr_solution.length; j++) {
-			if (arr_guess[i] === arr_solution[j]) {
-				arr_guess[i] = arr_solution[j] = "";
-				colors[i] = YELLOW;
-				break;
+		let colors = new Array(solution.length).fill("");
+		let arr_guess = [...guess];
+		let arr_solution = [...solution];
+
+		for (let i = 0; i < arr_solution.length; i++) {
+			if (arr_guess[i] === arr_solution[i]) {
+				arr_solution[i] = arr_guess[i] = "";
+				colors[i] = GREEN;
 			}
 		}
-	}
 
-	for (let i = 0; i < arr_guess.length; i++) {
-		if (arr_guess[i]) {
-			colors[i] = BLACK;
-		}
-	}
-
-	return colors;
-}
-
-exports.hardModeCheck = function(guess, guesses, guessColors) {	
-	for (let j in guessColors) {
-		let row = guessColors[j];
-		for (let i in row) {
-			if (row[i] === GREEN) {
-				if (guess[i] !== guesses[j][i]) {
-					return false;
-				}
-			} else if (row[i] === YELLOW) {
-				if (guess[i] === guesses[j][i] || !guess.includes(guesses[j][i])) {
-					return false;
+		for (let i = 0; i < arr_guess.length; i++) {
+			if (!arr_guess[i]) {
+				continue;
+			}
+			for (let j = 0; j < arr_solution.length; j++) {
+				if (arr_guess[i] === arr_solution[j]) {
+					arr_guess[i] = arr_solution[j] = "";
+					colors[i] = YELLOW;
+					break;
 				}
 			}
 		}
-	}
-	return true;
-}
 
-exports.RED = RED;
-exports.YELLOW = YELLOW;
-exports.GREEN = GREEN;
-exports.BLACK = BLACK;
-exports.WHITE = WHITE;
+		for (let i = 0; i < arr_guess.length; i++) {
+			if (arr_guess[i]) {
+				colors[i] = BLACK;
+			}
+		}
+
+		return colors;
+	}
+
+	exports.hardModeCheck = function(guess, guesses, guessColors) {	
+		for (let j in guessColors) {
+			let row = guessColors[j];
+			for (let i in row) {
+				if (row[i] === GREEN) {
+					if (guess[i] !== guesses[j][i]) {
+						return false;
+					}
+				} else if (row[i] === YELLOW) {
+					if (guess[i] === guesses[j][i] || !guess.includes(guesses[j][i])) {
+						return false;
+					}
+				}
+			}
+		}
+		return true;
+	}
+
+	exports.RED = RED;
+	exports.YELLOW = YELLOW;
+	exports.GREEN = GREEN;
+	exports.BLACK = BLACK;
+	exports.WHITE = WHITE;
+})(typeof exports === 'undefined'? this['mymodule']={}: exports);
